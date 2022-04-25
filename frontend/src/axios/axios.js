668532fe073615efaf030ac924ceb6ab1e1dfa64
bojan-suklev <96276041+bojan-suklev@users.noreply.github.com>
2022-04-25 21:56:09 +0200

    After spring deployment

--- frontend/src/axios/axios.js
+++ frontend/src/axios/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://localhost:9090/api',
+    baseURL: 'https://emtlab2frontendreact.herokuapp.com/api',
     headers: {
         'Access-Control-Allow-Origin' : '*',
        // 'Authorization': localStorage.getItem("JWT")
